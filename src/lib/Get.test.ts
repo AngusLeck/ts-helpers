a4fd2406588153ad8d92ff7f11b59ab527460117
Angus Leck <aleck42@gmail.com>
2022-09-24 12:40:40 +1000

    fix: make Get.test.ts run

--- src/lib/Get.test.ts
+++ src/lib/Get.test.ts
@@ -1,93 +1,97 @@
 import { assertEqual } from "./testHelpers";
 import { GET, Get } from "./Get";
 
-interface OneKey {
-  a: "a";
-}
+describe("Get", () => {
+  it("should return the type of the value at a path of T", () => {
+    interface OneKey {
+      a: "a";
+    }
 
-assertEqual<Get<OneKey, "a">, "a">(true);
+    assertEqual<Get<OneKey, "a">, "a">(true);
 
-interface MultipleKeys {
-  a: "a";
-  b: "b";
-  c: "c";
-}
+    interface MultipleKeys {
+      a: "a";
+      b: "b";
+      c: "c";
+    }
 
-assertEqual<Get<MultipleKeys, "a">, "a">(true);
-assertEqual<Get<MultipleKeys, "b">, "b">(true);
-assertEqual<Get<MultipleKeys, "c">, "c">(true);
+    assertEqual<Get<MultipleKeys, "a">, "a">(true);
+    assertEqual<Get<MultipleKeys, "b">, "b">(true);
+    assertEqual<Get<MultipleKeys, "c">, "c">(true);
 
-interface NestedKeys {
-  a: { b: { c: "c" } };
-}
+    interface NestedKeys {
+      a: { b: { c: "c" } };
+    }
 
-assertEqual<Get<NestedKeys, "a">, { b: { c: "c" } }>(true);
-assertEqual<Get<NestedKeys, "a.b">, { c: "c" }>(true);
-assertEqual<Get<NestedKeys, "a.b.c">, "c">(true);
+    assertEqual<Get<NestedKeys, "a">, { b: { c: "c" } }>(true);
+    assertEqual<Get<NestedKeys, "a.b">, { c: "c" }>(true);
+    assertEqual<Get<NestedKeys, "a.b.c">, "c">(true);
 
-type Union = { a: "a" } | { b: "b" };
+    type Union = { a: "a" } | { b: "b" };
 
-assertEqual<Get<Union, "a">, "a" | undefined>(true);
-assertEqual<Get<Union, "b">, "b" | undefined>(true);
+    assertEqual<Get<Union, "a">, "a" | undefined>(true);
+    assertEqual<Get<Union, "b">, "b" | undefined>(true);
 
-interface OptionalKey {
-  a?: "a";
-}
+    interface OptionalKey {
+      a?: "a";
+    }
 
-assertEqual<Get<OptionalKey, "a">, "a" | undefined>(true);
+    assertEqual<Get<OptionalKey, "a">, "a" | undefined>(true);
 
-type UnionWithMatchingKeys = { a: "a" } | { a: "b" };
+    type UnionWithMatchingKeys = { a: "a" } | { a: "b" };
 
-assertEqual<Get<UnionWithMatchingKeys, "a">, "a" | "b">(true);
+    assertEqual<Get<UnionWithMatchingKeys, "a">, "a" | "b">(true);
 
-interface NestedOptionalKeys {
-  a?: { b?: { c?: "c" } };
-}
+    interface NestedOptionalKeys {
+      a?: { b?: { c?: "c" } };
+    }
 
-assertEqual<Get<NestedOptionalKeys, "a">, { b?: { c?: "c" } } | undefined>(
-  true
-);
-assertEqual<Get<NestedOptionalKeys, "a.b">, { c?: "c" } | undefined>(true);
-assertEqual<Get<NestedOptionalKeys, "a.b.c">, "c" | undefined>(true);
+    assertEqual<Get<NestedOptionalKeys, "a">, { b?: { c?: "c" } } | undefined>(
+      true
+    );
+    assertEqual<Get<NestedOptionalKeys, "a.b">, { c?: "c" } | undefined>(true);
+    assertEqual<Get<NestedOptionalKeys, "a.b.c">, "c" | undefined>(true);
 
-interface InfinitelyNested {
-  a: InfinitelyNested;
-}
+    interface InfinitelyNested {
+      a: InfinitelyNested;
+    }
 
-assertEqual<GET<InfinitelyNested, "a.a.a.a.a">, InfinitelyNested>(true);
+    assertEqual<GET<InfinitelyNested, "a.a.a.a.a">, InfinitelyNested>(true);
 
-interface NestedMultipleKeys {
-  a: { b: { c: "c"; d: "d"; f: "f" } };
-}
+    interface NestedMultipleKeys {
+      a: { b: { c: "c"; d: "d"; f: "f" } };
+    }
 
-assertEqual<Get<NestedMultipleKeys, "a.b.c" | "a.b.d">, "c" | "d">(true);
+    assertEqual<Get<NestedMultipleKeys, "a.b.c" | "a.b.d">, "c" | "d">(true);
 
-interface ReadOnly {
-  readonly a: "a";
-}
+    interface ReadOnly {
+      readonly a: "a";
+    }
 
-assertEqual<Get<ReadOnly, "a">, "a">(true);
+    assertEqual<Get<ReadOnly, "a">, "a">(true);
 
-interface NestedReadOnly {
-  readonly a: { readonly b: { readonly c: "c" } };
-}
+    interface NestedReadOnly {
+      readonly a: { readonly b: { readonly c: "c" } };
+    }
 
-assertEqual<Get<NestedReadOnly, "a.b.c">, "c">(true);
-assertEqual<Get<NestedReadOnly, "a.b">, { readonly c: "c" }>(true);
-assertEqual<Get<NestedReadOnly, "a">, { readonly b: { readonly c: "c" } }>(
-  true
-);
+    assertEqual<Get<NestedReadOnly, "a.b.c">, "c">(true);
+    assertEqual<Get<NestedReadOnly, "a.b">, { readonly c: "c" }>(true);
+    assertEqual<Get<NestedReadOnly, "a">, { readonly b: { readonly c: "c" } }>(
+      true
+    );
 
-interface NestedOptionalReadOnly {
-  readonly a?: { readonly b?: { readonly c?: "c" } };
-}
+    interface NestedOptionalReadOnly {
+      readonly a?: { readonly b?: { readonly c?: "c" } };
+    }
 
-assertEqual<Get<NestedOptionalReadOnly, "a.b.c">, "c" | undefined>(true);
-assertEqual<
-  Get<NestedOptionalReadOnly, "a.b">,
-  { readonly c?: "c" } | undefined
->(true);
-assertEqual<
-  Get<NestedOptionalReadOnly, "a">,
-  { readonly b?: { readonly c?: "c" } } | undefined
->(true);
+    assertEqual<Get<NestedOptionalReadOnly, "a.b.c">, "c" | undefined>(true);
+    assertEqual<
+      Get<NestedOptionalReadOnly, "a.b">,
+      { readonly c?: "c" } | undefined
+    >(true);
+    assertEqual<
+      Get<NestedOptionalReadOnly, "a">,
+      { readonly b?: { readonly c?: "c" } } | undefined
+    >(true);
+  });
+});
