4c3c694c5a148727e9f7c9eb73b5998a894c1fc8
Angus Leck <aleck42@gmail.com>
2022-09-24 12:17:36 +1000

    fix: validate script

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,4 +7,29 @@ module.exports = {
     node: true,
     jest: true,
   },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "plugin:etc/recommended"
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "etc"],
+  parserOptions: {
+    ecmaVersion: 2019,
+    project: "./tsconfig.json",
+    sourceType: "module"
+  },
+  rules: {
+    indent: "off",
+    "@typescript-eslint/no-unused-vars": "error",
+    "@typescript-eslint/no-explicit-any": "error",
+    "@typescript-eslint/explicit-function-return-type": "error",
+    "no-trailing-spaces": "error",
+    "prefer-destructuring": "error",
+    "etc/prefer-interface": ["error", { "allowIntersection": false }],
+    "etc/prefer-less-than": "warn",
+    "etc/throw-error": "error"
+  }
 };
+
--- src/lib/Get.test.ts
+++ src/lib/Get.test.ts
@@ -1,17 +1,25 @@
 import { assertEqual } from "./testHelpers";
 import { GET, Get } from "./Get";
 
-type OneKey = { a: "a" };
+interface OneKey {
+  a: "a";
+}
 
 assertEqual<Get<OneKey, "a">, "a">(true);
 
-type MultipleKeys = { a: "a"; b: "b"; c: "c" };
+interface MultipleKeys {
+  a: "a";
+  b: "b";
+  c: "c";
+}
 
 assertEqual<Get<MultipleKeys, "a">, "a">(true);
 assertEqual<Get<MultipleKeys, "b">, "b">(true);
 assertEqual<Get<MultipleKeys, "c">, "c">(true);
 
-type NestedKeys = { a: { b: { c: "c" } } };
+interface NestedKeys {
+  a: { b: { c: "c" } };
+}
 
 assertEqual<Get<NestedKeys, "a">, { b: { c: "c" } }>(true);
 assertEqual<Get<NestedKeys, "a.b">, { c: "c" }>(true);
@@ -22,7 +30,9 @@ type Union = { a: "a" } | { b: "b" };
 assertEqual<Get<Union, "a">, "a" | undefined>(true);
 assertEqual<Get<Union, "b">, "b" | undefined>(true);
 
-type OptionalKey = { a?: "a" };
+interface OptionalKey {
+  a?: "a";
+}
 
 assertEqual<Get<OptionalKey, "a">, "a" | undefined>(true);
 
@@ -30,7 +40,9 @@ type UnionWithMatchingKeys = { a: "a" } | { a: "b" };
 
 assertEqual<Get<UnionWithMatchingKeys, "a">, "a" | "b">(true);
 
-type NestedOptionalKeys = { a?: { b?: { c?: "c" } } };
+interface NestedOptionalKeys {
+  a?: { b?: { c?: "c" } };
+}
 
 assertEqual<Get<NestedOptionalKeys, "a">, { b?: { c?: "c" } } | undefined>(
   true
@@ -38,19 +50,27 @@ assertEqual<Get<NestedOptionalKeys, "a">, { b?: { c?: "c" } } | undefined>(
 assertEqual<Get<NestedOptionalKeys, "a.b">, { c?: "c" } | undefined>(true);
 assertEqual<Get<NestedOptionalKeys, "a.b.c">, "c" | undefined>(true);
 
-type InfinitelyNested = { a: InfinitelyNested };
+interface InfinitelyNested {
+  a: InfinitelyNested;
+}
 
 assertEqual<GET<InfinitelyNested, "a.a.a.a.a">, InfinitelyNested>(true);
 
-type NestedMultipleKeys = { a: { b: { c: "c"; d: "d"; f: "f" } } };
+interface NestedMultipleKeys {
+  a: { b: { c: "c"; d: "d"; f: "f" } };
+}
 
 assertEqual<Get<NestedMultipleKeys, "a.b.c" | "a.b.d">, "c" | "d">(true);
 
-type ReadOnly = { readonly a: "a" };
+interface ReadOnly {
+  readonly a: "a";
+}
 
 assertEqual<Get<ReadOnly, "a">, "a">(true);
 
-type NestedReadOnly = { readonly a: { readonly b: { readonly c: "c" } } };
+interface NestedReadOnly {
+  readonly a: { readonly b: { readonly c: "c" } };
+}
 
 assertEqual<Get<NestedReadOnly, "a.b.c">, "c">(true);
 assertEqual<Get<NestedReadOnly, "a.b">, { readonly c: "c" }>(true);
@@ -58,9 +78,9 @@ assertEqual<Get<NestedReadOnly, "a">, { readonly b: { readonly c: "c" } }>(
   true
 );
 
-type NestedOptionalReadOnly = {
+interface NestedOptionalReadOnly {
   readonly a?: { readonly b?: { readonly c?: "c" } };
-};
+}
 
 assertEqual<Get<NestedOptionalReadOnly, "a.b.c">, "c" | undefined>(true);
 assertEqual<
