699f5959b7964fc336784740ea75b41ba9a88859
angus leck <aleck42@gmail.com>
2025-12-07 17:08:30 +1100

    ref: rm isDynamicArray

--- src/lib/IsDynamicArray.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Check if a type is a dynamic array (not a tuple).
- * Dynamic arrays have flexible length (number extends T["length"]).
- */
-export type IsDynamicArray<T> = T extends readonly unknown[]
-  ? number extends T["length"]
-    ? true
-    : false
-  : false;
